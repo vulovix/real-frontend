2f5c581dfa74246307c12b822f7d5a2f57fb2f2b
Ivan Vulović <ivan@Ivans-MacBook-Pro.local>
2025-11-01 22:09:55 +0100

    Removed unused imports

--- src/routes/Thunk/index.tsx
+++ src/routes/Thunk/index.tsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import RouteLayout from '../../components/Layout/RouteLayout';
 import { ThunkSidebar } from '../../components/UI/Sidebars';
-import { Button, Container, Group, Paper, Text, Title } from '@mantine/core';
-import { useDispatch, useSelector } from 'react-redux';
-import { AppDispatch, RootState } from '../../store';
 import Feature2Page from '../../features/Feature2';
 
 const ThunkRoute: React.FC = () => {
